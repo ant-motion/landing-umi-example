327e593c1771b9e4b29b5abe3c4334681b6e012b
jljsj33 <liangjun.jiang@alipay.com>
2018-10-31 15:41:24 +0800

    add landing template

---
+++ src/Home/Nav0.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import TweenOne from 'rc-tween-one';
+import { Menu } from 'antd';
+
+const Item = Menu.Item;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneOpen: false,
+      menuHeight: 0,
+    };
+  }
+
+  phoneClick = () => {
+    const menu = findDOMNode(this.menu);
+    const phoneOpen = !this.state.phoneOpen;
+    this.setState({
+      phoneOpen,
+      menuHeight: phoneOpen ? menu.scrollHeight : 0,
+    });
+  };
+
+  render() {
+    const { ...props } = this.props;
+    const { dataSource, isMobile } = props;
+    delete props.dataSource;
+    delete props.isMobile;
+    const { menuHeight, phoneOpen } = this.state;
+    const navData = dataSource.Menu.children;
+    const navChildren = Object.keys(navData).map((key, i) => (
+      <Item key={i.toString()} {...navData[key]}>
+        <a
+          {...navData[key].a}
+          href={navData[key].a.href}
+          target={navData[key].a.target}
+        >
+          {navData[key].a.children}
+        </a>
+      </Item>
+    ));
+    return (
+      <TweenOne
+        component="header"
+        animation={{ opacity: 0, type: 'from' }}
+        {...dataSource.wrapper}
+        {...props}
+      >
+        <div
+          {...dataSource.page}
+          className={`${dataSource.page.className}${phoneOpen ? ' open' : ''}`}
+        >
+          <TweenOne
+            animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }}
+            {...dataSource.logo}
+          >
+            <img width="100%" src={dataSource.logo.children} alt="img" />
+          </TweenOne>
+          {isMobile && (
+            <div
+              {...dataSource.mobileMenu}
+              onClick={() => {
+                this.phoneClick();
+              }}
+            >
+              <em />
+              <em />
+              <em />
+            </div>
+          )}
+          <TweenOne
+            {...dataSource.Menu}
+            animation={{ x: 30, type: 'from', ease: 'easeOutQuad' }}
+            ref={(c) => {
+              this.menu = c;
+            }}
+            style={isMobile ? { height: menuHeight } : null}
+          >
+            <Menu
+              mode={isMobile ? 'inline' : 'horizontal'}
+              defaultSelectedKeys={['0']}
+              theme={isMobile ? 'dark' : 'default'}
+            >
+              {navChildren}
+            </Menu>
+          </TweenOne>
+        </div>
+      </TweenOne>
+    );
+  }
+}
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from '../Home'
+
+export default function() {
+  return (
+    <Home />
+  );
+}
