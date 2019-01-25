728740cfedb322eff59311deba630c347f796b41
jljsj33 <liangjun.jiang@alipay.com>
2019-01-25 11:18:17 +0800

    fix https://github.com/ant-design/ant-design-landing/issues/25 and #1

--- .umirc.js
+++ .umirc.js
@@ -6,5 +6,8 @@ export default {
         antd: true,
       }
     ],
-  ]
+  ],
+  targets: {
+    ie: 11,
+  },
 }
--- src/pages/.umi/polyfills.js
+++ src/pages/.umi/polyfills.js
@@ -1,2 +1,5 @@
 import '@babel/polyfill';
 
+// Include this seperatly since it's not included in core-js
+// ref: https://github.com/zloirock/core-js/issues/117
+import '../../../../../../.npm-global/lib/node_modules/umi/node_modules/_url-polyfill@1.1.0@url-polyfill/url-polyfill.js';
